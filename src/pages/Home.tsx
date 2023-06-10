b072ff60eecaaa9a72233de56f9b210c728aa909
Italo Paixão Gomes <italo.gomes@mblabs.com.br>
2023-06-09 23:33:43 -0300

    add request to yu gi oh api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,46 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react/react-in-jsx-scope */
-import {View, Text, TouchableOpacity} from 'react-native';
+import { useEffect, useState } from 'react';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 
 export default function Home ({navigation}) {
 
+  const [cardInfo, setCardInfo] = useState<any>();
+
+  async function searchRandomCard () {
+    try{
+      const res = await fetch('https://db.ygoprodeck.com/api/v7/randomcard.php');
+      const json = await res.json();
+      setCardInfo(json);
+    } catch (err) {
+      setCardInfo('Houve um problema');
+    }
+  }
+
   return (
-    <View className="h-full flex justify-center items-center">
+    <View className="h-full flex justify-between items-center p-10">
       <View className="flex items-center gap-2">
-        <Text className="text-black text-3xl font-bold">Home</Text>
+        <Text className="text-black text-3xl font-bold">{navigation.getState().routeNames[navigation.getState().index]}</Text>
         <TouchableOpacity className="bg-gray-300 rounded py-2 px-4 justify-center items-center" onPress={() => navigation.navigate('Profile')}>
           <Text className='text-black text-lg'>
-            teste botão
+            Go to: Profile
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex gap-5">
+        <View>
+          {cardInfo?.card_images[0]?.image_url && (
+            <>
+            <Text>ad w dla çl</Text>
+            <Image className="h-72" source={{
+              uri: cardInfo.card_images[0].image_url,
+            }} />
+            </>
+          )}
+        </View>
+        <TouchableOpacity className="bg-gray-300 rounded py-2 px-4 justify-center items-center" onPress={searchRandomCard}>
+          <Text className='text-black text-lg'>
+            Search random card
           </Text>
         </TouchableOpacity>
       </View>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -7,10 +7,10 @@ export default function Profile ({navigation}) {
   return (
     <View className="h-full flex justify-center items-center">
       <View className="flex items-center gap-2">
-        <Text className="text-black text-3xl font-bold">Profile</Text>
+        <Text className="text-black text-3xl font-bold">{navigation.getState().routeNames[navigation.getState().index]}</Text>
         <TouchableOpacity className="bg-gray-300 rounded py-2 px-4 justify-center items-center" onPress={() => navigation.navigate('Home')}>
           <Text className='text-black text-lg'>
-            teste botão
+            Go to: Home
           </Text>
         </TouchableOpacity>
       </View>
